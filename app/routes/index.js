e0b259afda87b06436e904059b2d2c254fa9831b
Jeremy Fryd and Thomas Rivas <jeremy+thomas@>
2016-10-27 11:45:02 -0700

    fix missing adapter bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
-    });
   },
+
   actions: {
     savePost3(params){
       var newPost = this.store.createRecord('post', params);
